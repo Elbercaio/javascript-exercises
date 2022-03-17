30057eb044db7ce6df83b120164299ca0136fcd2
elbercaio <elbercaio@gmail.com>
2022-03-17 13:17:53 -0300

    11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const oldest = people.sort((a, b) =>
+    ("yearOfDeath" in a ? a.yearOfDeath : new Date().getFullYear()) -
+      a.yearOfBirth >
+    ("yearOfDeath" in b ? b.yearOfDeath : new Date().getFullYear()) -
+      b.yearOfBirth
+      ? -1
+      : 1
+  );
+  return oldest[0];
 };
 
 // Do not edit below this line
