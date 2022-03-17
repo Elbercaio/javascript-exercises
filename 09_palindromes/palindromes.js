d4e19ee22ac3722991aa48f37ebd16e9727d9142
elbercaio <elbercaio@gmail.com>
2022-03-17 11:13:35 -0300

    9 and 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let bareString = string
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s+]/g, "")
+    .toLowerCase();
+  let reversed = bareString.split("").reverse().join("");
+  if (reversed === bareString) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (number < 0) {
+    return "OOPS";
+  }
+  let firstTerm = 1;
+  let secondTerm = 1;
+  for (let i = 2; i < number; i++) {
+    [firstTerm, secondTerm] = [secondTerm, secondTerm + firstTerm];
+  }
+  return secondTerm;
 };
 
 // Do not edit below this line
